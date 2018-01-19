19da7fb39db53d0a9a8d8d4657500757bd653105
Kawika-Desktop <kawika-desktop@Kawika-Desktops-iMac.local>
2018-01-19 15:27:54 +0530

    Initial Commit

---
+++ SocketIO Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SocketIO Swift
+//
+//  Created by Kawika-Desktop on 19/01/18.
+//  Copyright © 2018 Kawika. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
