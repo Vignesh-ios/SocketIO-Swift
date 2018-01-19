2d6b67a21599cc87629a5f101e8962fdc32e9f49
Kawika-Desktop <kawika-desktop@kawika-desktops-imac.local>
2018-01-19 18:18:01 +0530

    scheme shared

--- SocketIO Swift/ViewController.swift
+++ SocketIO Swift/ViewController.swift
@@ -7,14 +7,34 @@
 //
 
 import UIKit
-
+import SocketIO
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        initSocket()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    func initSocket(){
+        let manager = SocketManager(socketURL: URL(string: "http://127.0.0.1:3000/")!, config: [.log(true), .compress])
+        let socket = manager.defaultSocket
+        
+        socket.on(clientEvent: .connect) {data, ack in
+            print("socket connected")
+        }
+        
+        socket.on("profile_name_changed") {data, ack in
+            guard let cur = data[0] as? Double else { return }
+            
+//            socket.emitWithAck("canUpdate", cur).timingOut(after: 0) {data in
+//                socket.emit("update", ["amount": cur + 2.50])
+//            }
+            
+            ack.with("Got your currentAmount", "dude")
+        }
+        
+        socket.connect()
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
